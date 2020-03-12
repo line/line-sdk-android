b260666f64013561f81d30a9fce9af715cd148c4
Kuan-Wei <harvey.hayasi@gmail.com>
2020-03-12 12:09:17 +0800

    Fix current android UI tests (#63)
    Co-authored-by: kuanwei.lin <kuanwei.lin@linecorp.com>

--- app/src/androidTest/java/com/linecorp/linesdk/pageobjects/MenuFragmentPage.java
+++ app/src/androidTest/java/com/linecorp/linesdk/pageobjects/MenuFragmentPage.java
@@ -4,19 +4,11 @@ import com.linecorp.linesdktest.R;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.core.AllOf.allOf;
 
 public class MenuFragmentPage {
-
-    private static final String testChannelId  = "123456789";
-
-    public void inputChannelId() {
-        onView(allOf(withId(R.id.channel_id_edittext), isDisplayed())).perform(typeText(testChannelId));
-    }
-
     public void navigateToApisPage() {
         onView(allOf(withId(R.id.apis_btn), isDisplayed())).perform(click());
     }
