480ce0b095b71abcc516c3a3c28cb637e77ef884
hayasilin <harvey.hayasi@gmail.com>
2020-03-13 18:32:01 +0800

    fix spacing

--- app/src/androidTest/java/com/linecorp/linesdk/pageobjects/LineAppLoginPage.java
+++ app/src/androidTest/java/com/linecorp/linesdk/pageobjects/LineAppLoginPage.java
@@ -12,6 +12,7 @@ import static junit.framework.TestCase.assertTrue;
 
 public class LineAppLoginPage {
     private UiDevice uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
     public void tapAllowButton() {
         uiDevice.wait(Until.hasObject(By.pkg(AndroidTestsConfig.LINE_PACKAGE_NAME).depth(0)),
                 AndroidTestsConfig.TIMEOUT);
