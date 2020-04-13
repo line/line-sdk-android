b678db95b47bb769e1216d5fa9e8d4bc12a0ff6d
Wouter Veeken <wouter.veeken@linecorp.com>
2020-04-13 16:49:10 +0900

    Hide class Constants
    See Slack thread: 
    https://line-enterprise.slack.com/archives/CAXAGE3T7/p1586757819052500

--- line-sdk/src/main/java/com/linecorp/linesdk/Constants.java
+++ line-sdk/src/main/java/com/linecorp/linesdk/Constants.java
@@ -1,5 +1,8 @@
 package com.linecorp.linesdk;
 
+/**
+ * @hide
+ * */
 public class Constants {
     public static final String LINE_APP_PACKAGE_NAME = "jp.naver.line.android";
     public static final String OPENID_DISCOVERY_DOCUMENT_URL = "https://access.line.me/.well-known/openid-configuration";
