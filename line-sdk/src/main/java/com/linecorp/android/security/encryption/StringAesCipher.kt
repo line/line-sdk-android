8ca87fb8a2f82f68ba30f19808c38ceb83a0a8d2
EricLu <eric.lu@linecorp.com>
2023-08-22 17:18:36 +0800

    dev: create empty class: StringAesCipher.kt
    Signed-off-by: EricLu <eric.lu@linecorp.com>

---
+++ line-sdk/src/main/java/com/linecorp/android/security/encryption/StringAesCipher.kt
@@ -0,0 +1,20 @@
+package com.linecorp.android.security.encryption
+
+import android.content.Context
+
+/**
+ * AES cipher by AndroidKeyStore
+ */
+class StringAesCipher : StringCipher {
+    override fun initialize(context: Context) {
+        TODO("Not yet implemented")
+    }
+
+    override fun encrypt(context: Context, plainText: String): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun decrypt(context: Context, b64CipherText: String): String {
+        TODO("Not yet implemented")
+    }
+}
