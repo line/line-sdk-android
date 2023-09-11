5536debee3c58dfebb8e18819f590618dabe83be
EricLu <eric.lu@linecorp.com>
2023-09-11 09:58:42 +0800

    dev: calculate HMAC value and store it in CipherData

--- line-sdk/src/main/java/com/linecorp/android/security/encryption/CipherData.kt
+++ line-sdk/src/main/java/com/linecorp/android/security/encryption/CipherData.kt
@@ -3,26 +3,34 @@ package com.linecorp.android.security.encryption
 import android.util.Base64
 
 class CipherData(
-    val encryptedByteArray: ByteArray,
-    val initialVector: ByteArray
+    val encryptedData: ByteArray,
+    val initialVector: ByteArray,
+    val hmacValue: ByteArray,
 ) {
-    override fun toString(): String = "${encryptedByteArray.encodeBase64()}$SEPARATOR$initialVector"
+    fun toCipherDataString(): String =
+        "${encryptedData.encodeBase64()}$SEPARATOR" +
+            "${initialVector.encodeBase64()}$SEPARATOR" +
+            hmacValue.encodeBase64()
 
     companion object {
         private const val SEPARATOR = ";"
-        private const val INDEX_ENCRYPTED_STRING = 0
+        private const val INDEX_ENCRYPTED_DATA = 0
         private const val INDEX_IV = 1
-        private const val SIZE = 2
+        private const val INDEX_HMAC = 2
+        private const val SIZE = 3
 
-        @Throws(IllegalArgumentException::class)
-        fun from(encryptedData: String): CipherData =
-            encryptedData
+        fun from(cipherDataString: String): CipherData =
+            cipherDataString
                 .split(SEPARATOR)
                 .takeIf { it.size == SIZE }
                 ?.run {
-                    CipherData(get(INDEX_ENCRYPTED_STRING).decodeBase64(), get(INDEX_IV).decodeBase64())
+                    CipherData(
+                        encryptedData = get(INDEX_ENCRYPTED_DATA).decodeBase64(),
+                        initialVector = get(INDEX_IV).decodeBase64(),
+                        hmacValue = get(INDEX_HMAC).decodeBase64()
+                    )
                 }
-                ?: throw IllegalArgumentException("Failed to split encrypted text `$encryptedData`")
+                ?: throw IllegalArgumentException("Failed to split encrypted text `$cipherDataString`")
     }
 }
 
--- line-sdk/src/main/java/com/linecorp/android/security/encryption/StringAesCipher.kt
+++ line-sdk/src/main/java/com/linecorp/android/security/encryption/StringAesCipher.kt
@@ -3,9 +3,14 @@ package com.linecorp.android.security.encryption
 import android.content.Context
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
+import android.security.keystore.KeyProperties.PURPOSE_DECRYPT
+import android.security.keystore.KeyProperties.PURPOSE_ENCRYPT
+import android.security.keystore.KeyProperties.PURPOSE_SIGN
+import android.security.keystore.KeyProperties.PURPOSE_VERIFY
 import java.security.KeyStore
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
+import javax.crypto.Mac
 import javax.crypto.SecretKey
 import javax.crypto.spec.IvParameterSpec
 
@@ -13,96 +18,141 @@ import javax.crypto.spec.IvParameterSpec
  * AES cipher by AndroidKeyStore
  */
 class StringAesCipher : StringCipher {
-
     private val keyStore: KeyStore by lazy {
         KeyStore.getInstance(ANDROID_KEY_STORE).also {
             it.load(null)
         }
     }
 
+    private lateinit var hmac: Mac
+
     override fun initialize(context: Context) {
+        if (!::hmac.isInitialized) {
+            getAesSecretKey()
+            val integrityKey = getIntegrityKey()
+
+            hmac = Mac.getInstance(KeyProperties.KEY_ALGORITHM_HMAC_SHA256).apply {
+                init(integrityKey)
+            }
+        }
     }
 
     override fun encrypt(context: Context, plainText: String): String {
-        val secretKey = try {
-            getSecretKey()
-        } catch (e: Exception) {
-            throw EncryptionException("Failed to retrieve secret key!", e)
-        }
+        initialize(context)
 
-        val cipher = Cipher.getInstance(TRANSFORMATION_FORMAT).apply {
-            init(Cipher.ENCRYPT_MODE, secretKey)
-        }
-        val encryptedByteArray = try {
-            cipher.doFinal(plainText.toByteArray())
-        } catch (e: Exception) {
-            throw EncryptionException("Failed to encrypt!", e)
-        }
+        try {
+            val secretKey = getAesSecretKey()
 
-        return CipherData(encryptedByteArray, cipher.iv).toString()
+            val cipher = Cipher.getInstance(TRANSFORMATION_FORMAT).apply {
+                init(Cipher.ENCRYPT_MODE, secretKey)
+            }
+            val encryptedData: ByteArray = cipher.doFinal(plainText.toByteArray())
+
+            return CipherData(
+                encryptedData = encryptedData,
+                initialVector = cipher.iv,
+                hmacValue = hmac.calculateHmacValue(encryptedData, cipher.iv)
+            ).toCipherDataString()
+        } catch (e: Exception) {
+            throw EncryptionException("Failed to encrypt", e)
+        }
     }
 
     override fun decrypt(context: Context, cipherText: String): String {
-        val secretKey = try {
-            getSecretKey()
-        } catch (e: Exception) {
-            throw EncryptionException("Failed to retrieve secret key!", e)
-        }
-        val cipherData = CipherData.from(cipherText)
-        val ivSpec = IvParameterSpec(cipherData.initialVector)
         try {
+            val secretKey = getAesSecretKey()
+
+            val cipherData = CipherData.from(cipherText)
+            val ivSpec = IvParameterSpec(cipherData.initialVector)
+
             return Cipher.getInstance(TRANSFORMATION_FORMAT)
                 .apply { init(Cipher.DECRYPT_MODE, secretKey, ivSpec) }
-                .run { doFinal(cipherData.encryptedByteArray) }
+                .run { doFinal(cipherData.encryptedData) }
                 .let {
                     String(it)
                 }
+
         } catch (e: Exception) {
-            throw EncryptionException("Failed to decrypt!", e)
+            throw EncryptionException("Failed to decrypt", e)
         }
     }
 
-    private fun getSecretKey(): SecretKey? {
-        if (!isAesKeyCreated()) {
+    private fun getAesSecretKey(): SecretKey {
+        return if (keyStore.containsAlias(AES_KEY_ALIAS)) {
+            val secretKeyEntry =
+                keyStore.getEntry(AES_KEY_ALIAS, null) as KeyStore.SecretKeyEntry
+
+            secretKeyEntry.secretKey
+        } else {
             createAesKey()
         }
-        val secretKeyEntry =
-            keyStore.getEntry(AES_KEY_ALIAS, null) as KeyStore.SecretKeyEntry
-
-        return secretKeyEntry.secretKey
     }
 
-    private fun isAesKeyCreated(): Boolean = keyStore.containsAlias(AES_KEY_ALIAS)
+    private fun getIntegrityKey(): SecretKey {
+        return if (keyStore.containsAlias(INTEGRITY_KEY_ALIAS)) {
+            val secretKeyEntry =
+                keyStore.getEntry(INTEGRITY_KEY_ALIAS, null) as KeyStore.SecretKeyEntry
 
-    private fun createAesKey() {
-        val keyGenerator = KeyGenerator.getInstance(AES_ALGORITHM_NAME, ANDROID_KEY_STORE)
+            secretKeyEntry.secretKey
+        } else {
+            createIntegrityKey()
+        }
+    }
+
+    /**
+     * Create AES key in AndroidKeyStore
+     */
+    private fun createAesKey(): SecretKey {
+        val keyGenerator = KeyGenerator
+            .getInstance(KeyProperties.KEY_ALGORITHM_AES, ANDROID_KEY_STORE)
         val keyGenParameterSpec = KeyGenParameterSpec.Builder(
             AES_KEY_ALIAS,
-            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+            PURPOSE_ENCRYPT or PURPOSE_DECRYPT /* or PURPOSE_VERIFY or PURPOSE_SIGN */
         )
-            .setKeySize(AES_KEY_SIZE_IN_BIT)
-            .setBlockModes(AES_KEY_BLOCK_MODE)
-            .setEncryptionPaddings(AES_KEY_PADDING)
+            .setKeySize(KEY_SIZE_IN_BIT)
+            .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
             .build()
 
         keyGenerator.run {
             init(keyGenParameterSpec)
-            generateKey()
+            return generateKey()
         }
     }
+
+    private fun createIntegrityKey(): SecretKey {
+        val keyGenerator = KeyGenerator
+            .getInstance(KeyProperties.KEY_ALGORITHM_HMAC_SHA256, ANDROID_KEY_STORE)
+        val keyGenParameterSpec = KeyGenParameterSpec.Builder(
+            INTEGRITY_KEY_ALIAS,
+            PURPOSE_VERIFY or PURPOSE_SIGN
+        )
+            .build()
+
+        keyGenerator.run {
+            init(keyGenParameterSpec)
+            return generateKey()
+        }
+    }
+
+    private fun Mac.calculateHmacValue(
+        encryptedData: ByteArray,
+        initialVector: ByteArray
+    ): ByteArray = doFinal(encryptedData + initialVector)
 
     companion object {
         private const val AES_KEY_ALIAS =
             "com.linecorp.android.security.encryption.StringAesCipher"
 
-        private const val ANDROID_KEY_STORE = "AndroidKeyStore"
-        private const val AES_ALGORITHM_NAME = KeyProperties.KEY_ALGORITHM_AES
-        private const val AES_KEY_BLOCK_MODE = KeyProperties.BLOCK_MODE_CBC
-        private const val AES_KEY_PADDING = KeyProperties.ENCRYPTION_PADDING_PKCS7
+        private const val INTEGRITY_KEY_ALIAS =
+            "com.linecorp.android.security.encryption.StringAesCipher.INTEGRITY_KEY"
 
-        private const val AES_KEY_SIZE_IN_BIT = 256
+        private const val ANDROID_KEY_STORE = "AndroidKeyStore"
+        private const val KEY_SIZE_IN_BIT = 256
 
         private const val TRANSFORMATION_FORMAT =
-            "$AES_ALGORITHM_NAME/$AES_KEY_BLOCK_MODE/$AES_KEY_PADDING"
+            KeyProperties.KEY_ALGORITHM_AES +
+                "/${KeyProperties.BLOCK_MODE_CBC}" +
+                "/${KeyProperties.ENCRYPTION_PADDING_PKCS7}"
     }
 }
