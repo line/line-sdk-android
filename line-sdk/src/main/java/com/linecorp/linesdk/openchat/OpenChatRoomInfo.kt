916004b699ef72f20e5c6a368f6236742116644a
EricLu <eric.lu@linecorp.com>
2025-09-23 09:39:45 +0800

    dev: use standalone kotlin-parcelize Gradle plugin

--- line-sdk/src/main/java/com/linecorp/linesdk/openchat/OpenChatRoomInfo.kt
+++ line-sdk/src/main/java/com/linecorp/linesdk/openchat/OpenChatRoomInfo.kt
@@ -1,7 +1,7 @@
 package com.linecorp.linesdk.openchat
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class OpenChatRoomInfo(
